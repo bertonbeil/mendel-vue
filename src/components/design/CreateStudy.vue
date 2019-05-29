03fec008f6ad8c7ebfcb60141012ebd4a8911429
Sviatoslav Panevnyk <spanevnyk@ukr.net>
2019-05-29 14:16:35 +0300

    Add draggable functionality in Study design modal

--- src/components/design/CreateStudy.vue
+++ src/components/design/CreateStudy.vue
@@ -15,6 +15,45 @@
             </el-form-item>
           </el-col>
         </el-row>
+
+        <el-row :gutter="20">
+          <el-col :span="24">
+            <h4>Collaborators:</h4>
+            <p>If you will have lab members working with you on this study, assign them here. Drag names from the left box to the right-hand ‘collaborators’ box to assign them as a collaborator. Study collaborators will have read and write access to all of the projects within this study.</p>
+          </el-col>
+        </el-row>
+
+        <el-row :gutter="20" class="mt-6">
+          <el-col :span="12">
+            <p class="text-xl">Investigators</p>
+          </el-col>
+          <el-col :span="12">
+            <p class="text-xl">Collaborators</p>
+          </el-col>
+          <el-col :span="24">
+            <p>Note: Investigators that you add here as collaborators will have read and write access to all of the projects and assemblies in this study</p>
+          </el-col>
+        </el-row>
+
+        <!-- Draggable zones -->
+        <el-row :gutter="20" class="mt-4 mb-8 flex">
+          <el-col :span="12">
+            <draggable class="p-3 bg-gray-100 rounded shadow-md" v-model="investigators" group="collaborators">
+              <div class="inline-block w-1/2 p-1 cursor-pointer"
+                v-for="investigator in investigators" :key="investigator">
+                <p class="m-1 p-1 bg-gray-300 rounded shadow-md">{{ investigator }}</p>
+              </div>
+            </draggable>
+          </el-col>
+          <el-col :span="12">
+            <draggable class="min-h-full p-3 bg-gray-100 rounded shadow-md" v-model="collaborators" group="collaborators">
+              <div class="inline-block w-1/2 p-1 cursor-pointer"
+                v-for="collaborator in collaborators" :key="collaborator">
+                <p class="m-1 p-1 bg-gray-300 rounded shadow-md">{{ collaborator }}</p>
+              </div>
+            </draggable>
+          </el-col>
+        </el-row>
       </el-form>
     </div>
     <!-- Modal action buttons -->
@@ -30,13 +69,22 @@
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { DialogBase } from '@/utils/interfaces'
 import { httpService } from '@/services/http.service'
-import { loader } from '@/utils/helpers'
+// import { loader } from '@/utils/helpers'
+import draggable from 'vuedraggable'
+
 @Component({
-  name: 'CreateStudy'
+  name: 'CreateStudy',
+  components: {
+    draggable
+  }
 })
+
 export default class CreateStudy extends Vue {
   @Prop({ required: true }) modalData!: DialogBase
   @Prop({ required: true }) isLoading!: boolean
+
+  investigators: any = []
+  collaborators: any = []
 
   studyForm: any = {
     name: '',
@@ -53,10 +101,14 @@ export default class CreateStudy extends Vue {
     studyForm: any
   }
 
+  created () {
+    this.getCollaboratorList()
+  }
+
   /* submit Modal data */
   save () {
     this.$refs['studyForm'].validate((valid: any) => {
-      if (valid) this.$emit('save', { new_study: this.studyForm, collaborators: [] }); else return false
+      if (valid) this.$emit('save', { new_study: this.studyForm, collaborators: this.collaborators }); else return false
     })
   }
 
@@ -65,12 +117,9 @@ export default class CreateStudy extends Vue {
     this.$emit('loadOn')
     return httpService.get('query/collaboratorList')
       .then((res: any) => {
+        res.data.rows.map((investigator: any) => this.investigators.push(investigator.id))
         this.$emit('loadOff')
       }).catch((err: any) => { this.$emit('loadOff'); console.log(err) })
-  }
-
-  created () {
-    this.getCollaboratorList()
   }
 }
 </script>
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,33 +1,31 @@
 import axios from 'axios'
 
 class HttpService {
-
   /* */
-  get<T>(relativeUrl: string, params?: any): Promise<T> {
+  get<T> (relativeUrl: string, params?: any): Promise<T> {
     return axios.get(this.getUrl(relativeUrl), params ? { params } : undefined) as any
   }
 
-  put<T>(relativeUrl: string, payload?: any): Promise<T> {
+  put<T> (relativeUrl: string, payload?: any): Promise<T> {
     return axios.put(this.getUrl(relativeUrl), payload) as any
   }
 
-  post<T>(relativeUrl: string, payload?: any): Promise<T> {
+  post<T> (relativeUrl: string, payload?: any): Promise<T> {
     return axios.post(this.getUrl(relativeUrl), payload) as any
   }
 
-  delete<T>(relativeUrl: string): Promise<T> {
+  delete<T> (relativeUrl: string): Promise<T> {
     return axios.delete(this.getUrl(relativeUrl)) as any
   }
 
-  public getUrl(relativeUrl: string): string {
+  public getUrl (relativeUrl: string): string {
     relativeUrl = this.removeLeadingSlash(relativeUrl)
     return `${process.env.VUE_APP_API_URL}/${relativeUrl}`
   }
 
-  private removeLeadingSlash(url: string) {
+  private removeLeadingSlash (url: string) {
     return url.charAt(0) === '/' ? url.substr(1) : url
   }
-
 }
 
-export const httpService = new HttpService() 
+export const httpService = new HttpService()
