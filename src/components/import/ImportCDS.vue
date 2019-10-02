f2e542b3909d534040641bf4bc696d36ed307547
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-10-02 12:05:06 +0300

    re-create upload component

--- src/components/import/ImportCDS.vue
+++ src/components/import/ImportCDS.vue
@@ -119,8 +119,8 @@
           </el-col>
           <el-col :span="24">
             <h4 class="text-xl text-black mt-3">Upload fasta:</h4>
-            <UploadFile @getUploadFile='file => importCDSForm.dna_sequence = file' :accept='".fa,.fasta"' />
-          </el-col>
+            <UploadFile :dna-sequence.sync="importCDSForm.dna_sequence"  :accept='".fa,.fasta"' />
+           </el-col>
         </el-row>
       </el-form>
     </div>
