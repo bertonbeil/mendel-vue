8a76c589da02bb08906f445eb2c6d2ca2520f0d5
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-10-04 11:54:04 +0300

    create segment details fix minor bug

--- src/components/import/ImportCustomParts.vue
+++ src/components/import/ImportCustomParts.vue
@@ -3,24 +3,36 @@
     <div class="custom-parts">
       <el-row :gutter="20" class="mb-20">
         <el-col :span="24">
-          <h3 class="text-black font-bold">Import Custom Parts</h3>
+          <h3 class="text-black font-bold" v-if="!showSegmentDetails">Import Custom Parts</h3>
+          <h3 class="text-black font-bold" v-else>Segment Details</h3>
         </el-col>
         <el-col :span="24">
-          <p v-html="modalData.dialogIntro" class="mb-8 break-word"></p>
+          <p v-html="modalData.dialogIntro" class="mb-8 break-word" v-if="!showSegmentDetails"></p>
+          <p
+            class="mb-8 break-word"
+            v-else
+          >This page allows you to specify a grant that is related to each segment. This page also allows you to specify the resistance marker for the vector the segment is delivered in.</p>
         </el-col>
       </el-row>
       <!-- Main modal content -->
       <el-row :gutter="20" v-if="!showSegmentDetails">
         <el-col :span="24">
-          <el-form :model="importCustomPartsForm" label-position="top" :rules="rules" ref="importCustomPartsForm">
+          <el-form
+            :model="importCustomPartsForm"
+            label-position="top"
+            :rules="rules"
+            ref="importCustomPartsForm"
+          >
             <el-form-item label="Order name:" prop="projectName">
-              <p class="text-sm text-gray-200 mb-20">Specify a unique order name for the set of segments you are ordering. For example, you could use the date and your name.</p>
+              <p
+                class="text-sm text-gray-200 mb-20"
+              >Specify a unique order name for the set of segments you are ordering. For example, you could use the date and your name.</p>
               <el-input
                 type="text"
                 v-model="importCustomPartsForm.projectName"
                 @change="$emit('loadOf')"
-                placeholder="Type order name..">
-              </el-input>
+                placeholder="Type order name.."
+              ></el-input>
             </el-form-item>
             <el-form-item label="Order description:">
               <el-input type="textarea" placeholder="Type optional order description" :rows="4"></el-input>
@@ -31,8 +43,8 @@
                 v-model="importCustomPartsForm.data"
                 placeholder="Alternatively you can paste the locus sequence in fasta format"
                 resize="none"
-                :rows="4">
-              </el-input>
+                :rows="4"
+              ></el-input>
             </el-form-item>
             <el-col :span="24">
               <h4 class="text-xl text-black mt-3">Upload fasta:</h4>
@@ -65,9 +77,8 @@
             :key="index"
             :segmentData.sync="importCustomPartsForm.data[index]"
             :grants="grants"
-            :orderName="importCustomPartsForm.projectName">
-          </SegmentDetailsRow>
-          {{ importCustomPartsForm.data }}
+            :orderName="importCustomPartsForm.projectName"
+          ></SegmentDetailsRow>
         </el-col>
       </el-row>
 
@@ -78,7 +89,7 @@
       </div>
       <div slot="footer" v-else class="text-center">
         <el-button type="danger" @click="backToCustomParts">Cancel and back</el-button>
-        <el-button type="primary" @click="sendDataForReview">Submit for rewiew</el-button>
+        <el-button type="primary" @click="save">Submit for rewiew</el-button>
       </div>
     </div>
   </div>
@@ -88,6 +99,7 @@
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { DialogBase, ImportsCustomParts } from '@/utils/interfaces'
 import { httpService } from '@/services/http.service'
+import { alertMixin } from '@/utils/mixins'
 
 @Component({ name: 'ImportCustomParts' })
 
@@ -113,24 +125,13 @@ export default class ImportCustomParts extends Vue {
     importCustomPartsForm: HTMLFormElement
   }
 
-  sendDataForReview () {
-    this.$emit('loadOn')
-    return httpService.post('query/qinglanAssemblyDesigner', this.sendData)
-      .then((res: any) => console.log(res.data))
-      .catch((err: any) => { throw new Error(err) })
-      .finally(() => this.$emit('loadOff'))
-  }
-
   get sendData () {
     return this.importCustomPartsForm
   }
 
   /* submit Modal data */
   save () {
-    this.$refs['importCustomPartsForm'].validate((valid: boolean) => {
-      if (valid) this.$emit('save', { data: this.sendData })
-      else return false
-    })
+    this.$emit('save', { data: this.sendData })
   }
 
   /* Get segment data */
