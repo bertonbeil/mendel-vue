bf18dd93cdce9847052034199f2a2c004be97aa2
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-10-05 18:56:41 +0300

    create uload file indicator

--- src/components/shared/Upload/UploadFile.vue
+++ src/components/shared/Upload/UploadFile.vue
@@ -1,26 +1,32 @@
+
 <template>
-  <el-row>
-    <el-col :span="24" class="mt-10 flex items-center">
-      <label for="uploadFileInput" class="upLoad-file-input">Click to upload</label>
-      <input
-        type="file"
-        @change="uploadFile"
-        :file-list="fileList"
-        :accept="accept"
-        id="uploadFileInput"
-        class="hidden"/>
-      <el-button
-        circle
-        v-if="!updateImmediately"
-        title="show sequence"
-        icon="el-icon-view"
-        type="button"
-        size="small"
-        @click="showSequence"
-        class="ml-10">
-      </el-button>
-    </el-col>
-  </el-row>
+      <div class="mt-10 flex items-center" :class="isFileLoading">
+        <label for="uploadFileInput" :class="{'cursor-wait':fileLoading}" class="upLoad-file-input">Click to upload</label>
+          <input
+            type="file"
+            @change="uploadFile"
+            :file-list="fileList"
+            :accept="accept"
+            :disabled="fileLoading"
+            id="uploadFileInput"
+            class="hidden"/>
+          <el-button
+            circle
+            v-if="!updateImmediately"
+            title="show sequence"
+            icon="el-icon-view"
+            type="button"
+            size="small"
+            :class="{'cursor-wait':fileLoading}"
+            @click="showSequence"
+            class="ml-10">
+          </el-button>
+          <p v-if="fileName" class="ml-10">
+            <i class="el-icon-document"></i>
+            {{fileName}}
+          </p>
+    </div>
+
 </template>
 
 <script lang="ts">
@@ -34,18 +40,29 @@ export default class UploadFile extends Vue {
 
   result: null | string = null;
   fileList: object[] = [];
+  fileLoading: boolean = false;
+  fileName: string = '';
 
   showSequence () {
     this.$emit('update:dnaSequence', this.result)
   }
 
+  get isFileLoading () {
+    if (this.fileLoading) {
+      return 'cursor-wait opacity-50'
+    }
+  }
+
   uploadFile (e: any) {
+    this.fileLoading = true
     const fileReader = new FileReader()
     fileReader.readAsText(e.target.files[0])
+    this.fileName = e.target.files[0].name
     fileReader.onload = (e: any) => {
       this.result = e.target.result
       if (this.updateImmediately) this.$emit('update:dnaSequence', e.target.result)
     }
+    this.fileLoading = false
   }
 }
 </script>
