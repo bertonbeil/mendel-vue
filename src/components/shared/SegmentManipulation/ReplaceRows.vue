63fd969a261c5596ab69561e4516e56959224e1a
Sviatoslav <s.panevnyk@dev.softonix.org>
2019-10-15 16:37:56 +0300

    Minor fixes

--- src/components/shared/SegmentManipulation/ReplaceRows.vue
+++ src/components/shared/SegmentManipulation/ReplaceRows.vue
@@ -1,12 +1,21 @@
 <template>
   <el-row :gutter="20" class="mb-30">
     <el-col :span="5">
-      <el-select v-model="localReplaceRow.segment_type" @change="getCustomSegmentList" placeholder="Select type" class="w-full">
+      <el-select
+        v-model="localReplaceRow.segment_type"
+        @change="getCustomSegmentList"
+        placeholder="Select type"
+        class="w-full">
         <el-option v-for="item in typesList" :key="item.name" :label="item.name" :value="item.name"></el-option>
       </el-select>
     </el-col>
     <el-col :span="5">
-      <el-select v-model="localReplaceRow.name" @change="handleNameChange" placeholder="Select name" class="w-full">
+      <el-select
+        v-model="localReplaceRow.name"
+        @change="handleNameChange"
+        :disabled="!localReplaceRow.segment_type"
+        placeholder="Select name"
+        class="w-full">
         <el-option v-for="(item, i) in localCustomSegments" :key="i" :label="item.name" :value="item.name"></el-option>
       </el-select>
     </el-col>
@@ -17,7 +26,11 @@
       <el-input v-model="localReplaceRow.value" placeholder="Sequence" disabled></el-input>
     </el-col>
     <el-col :span="5">
-      <el-select v-model="localReplaceRow.direction" placeholder="Select direction" class="w-full">
+      <el-select
+        v-model="localReplaceRow.direction"
+        :disabled="!localReplaceRow.segment_type"
+        placeholder="Select direction"
+        class="w-full">
         <el-option v-for="item in directionList" :key="item.value" :label="item.label" :value="item.value"></el-option>
       </el-select>
     </el-col>
@@ -39,15 +52,15 @@ export default class ReplaceRows extends Vue {
 
   directionList: any[] = [ { label: '5`', value: '5' }, { label: '3`', value: '3' } ]
 
-  localReplaceRow = {}
-  localCustomSegments = []
+  localReplaceRow: any = {}
+  localCustomSegments: any = []
 
   getCustomSegmentList (value: any) {
-    return httpService.post('query/customSegmentList', {
-      study: this.studyName,
-      project: this.projectName,
-      type: value
-    }).then((res: any) => { this.localCustomSegments = res.data.rows })
+    return httpService.post('query/customSegmentList', { study: this.studyName, project: this.projectName, type: value })
+      .then((res: any) => {
+        this.localReplaceRow.name = ''
+        this.localCustomSegments = res.data.rows
+      })
       .catch((err: any) => { throw new Error(err) })
       .finally(() => this.$emit('loadOff'))
   }
