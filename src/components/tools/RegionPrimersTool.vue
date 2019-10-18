ab0ad54e5271f5ede1f5bd99f49f6d1018191a0a
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-10-18 16:58:45 +0300

    Clear inputs, delete unneeded code, refactoring

--- src/components/tools/RegionPrimersTool.vue
+++ src/components/tools/RegionPrimersTool.vue
@@ -5,30 +5,19 @@
         <el-col :span="24">
           <h3 class="text-black font-bold">Region primers tool</h3>
         </el-col>
-        <el-col :span="23" class="mb-40">
+        <el-col :span="24" class="mb-40">
           <p v-html="modalData.dialogIntro"></p>
         </el-col>
-        <el-form
-          :model="RegionPrimersToolForm"
-          label-position="top"
-          :rules="rules"
-          ref="RegionPrimersToolForm"
-        >
+        <el-form :model="regionPrimersToolForm" label-position="top" :rules="rules" ref="regionPrimersToolForm">
           <el-row :gutter="10" class="mb-30">
             <el-col :span="8">
               <el-form-item label="Study:" prop="studyName">
                 <el-select
-                  v-model="RegionPrimersToolForm.studyName"
+                  v-model="regionPrimersToolForm.studyName"
                   @change="getProjects"
                   placeholder="Select study"
-                  class="w-full"
-                >
-                  <el-option
-                    v-for="study in studyList"
-                    :key="study.name"
-                    :label="study.name"
-                    :value="study.name"
-                  ></el-option>
+                  class="w-full">
+                  <el-option v-for="study in studyList" :key="study.name" :label="study.name" :value="study.name"></el-option>
                 </el-select>
               </el-form-item>
             </el-col>
@@ -36,18 +25,12 @@
             <el-col :span="8">
               <el-form-item label="Project:" prop="projectName">
                 <el-select
-                  v-model="RegionPrimersToolForm.projectName"
+                  v-model="regionPrimersToolForm.projectName"
                   @change="getRegions"
                   placeholder="Select project"
                   class="w-full"
-                  :disabled="!RegionPrimersToolForm.studyName"
-                >
-                  <el-option
-                    v-for="project in projectList"
-                    :key="project.name"
-                    :label="project.name"
-                    :value="project.name"
-                  ></el-option>
+                  :disabled="!regionPrimersToolForm.studyName">
+                  <el-option v-for="project in projectList" :key="project.name" :label="project.name" :value="project.name"></el-option>
                 </el-select>
               </el-form-item>
             </el-col>
@@ -55,16 +38,11 @@
             <el-col :span="8">
               <el-form-item label="Region:" prop="dnaDesignName">
                 <el-select
-                  v-model="RegionPrimersToolForm.dnaDesignName"
+                  v-model="regionPrimersToolForm.dnaDesignName"
                   placeholder="Select region"
                   class="w-full"
-                  :disabled="!RegionPrimersToolForm.projectName"
-                >
-                  <el-option
-                    v-for="region in regionList"
-                    :key="region"
-                    :label="region"
-                    :value="region"
+                  :disabled="!regionPrimersToolForm.projectName">
+                  <el-option v-for="region in regionList" :key="region" :label="region" :value="region"
                   ></el-option>
                 </el-select>
               </el-form-item>
@@ -75,82 +53,46 @@
               <p>You can define your desired parameters for primer design here. The defaults should generally work well for most amplicons amplified with modern high-fidelity polymerases.</p>
             </el-col>
 
-            <el-col :span="6">
+            <el-col :span="5">
               <h5 class="text-black mb-20">Primer length</h5>
-              <el-form-item
-                label="Minimum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.minLength" class="ml-10"></el-input-number>
+              <el-form-item label="Minimum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.minLength" class="ml-10"></el-input-number>
               </el-form-item>
-              <el-form-item
-                label="Maximum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxLength" class="ml-10"></el-input-number>
+              <el-form-item label="Maximum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxLength" class="ml-10"></el-input-number>
               </el-form-item>
             </el-col>
 
-            <el-col :span="6">
+            <el-col :span="5">
               <h5 class="text-black mb-20">Annealing temperature</h5>
-              <el-form-item
-                label="Minimum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.minTemperature" class="ml-10"></el-input-number>
+              <el-form-item label="Minimum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.minTemperature" class="ml-10"></el-input-number>
               </el-form-item>
-              <el-form-item
-                label="Maximum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxTemperature" class="ml-10"></el-input-number>
+              <el-form-item label="Maximum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxTemperature" class="ml-10"></el-input-number>
               </el-form-item>
             </el-col>
 
-            <el-col :span="6">
+            <el-col :span="5">
               <h5 class="text-black mb-20">Percent GC</h5>
-              <el-form-item
-                label="Minimum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.minPercentGC" class="ml-10"></el-input-number>
+              <el-form-item label="Minimum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.minPercentGC" class="ml-10"></el-input-number>
               </el-form-item>
-              <el-form-item
-                label="Maximum:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxPercentGC" class="ml-10"></el-input-number>
+              <el-form-item label="Maximum:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxPercentGC" class="ml-10"></el-input-number>
               </el-form-item>
             </el-col>
 
-            <el-col :span="6">
+            <el-col :span="9">
               <h5 class="text-black mb-20">Base composition</h5>
-              <el-form-item
-                label="Maximum homopolymer length:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxHomopolymerLen" class="ml-10"></el-input-number>
+              <el-form-item label="Maximum homopolymer length:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxHomopolymerLen" class="ml-10"></el-input-number>
               </el-form-item>
-              <el-form-item
-                label="Minimum base representation:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxPercentGC" class="ml-10"></el-input-number>
+              <el-form-item label="Minimum base representation:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxPercentGC" class="ml-10"></el-input-number>
               </el-form-item>
-              <el-form-item
-                label="Maximum hairpin length:"
-                class="form-item-label flex items-center text-sm"
-                size="mini"
-              >
-                <el-input-number v-model="RegionPrimersToolForm.maxHairpinLen" class="ml-10"></el-input-number>
+              <el-form-item label="Maximum hairpin length:" class="form-item-label flex items-center text-sm" size="mini">
+                <el-input-number v-model="regionPrimersToolForm.maxHairpinLen" class="ml-10"></el-input-number>
               </el-form-item>
             </el-col>
           </el-row>
@@ -165,21 +107,23 @@
     </div>
   </div>
 </template>
+
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from 'vue-property-decorator'
 import { DialogBase, RegionPrimersTools } from '@/utils/interfaces'
 import { httpService } from '@/services/http.service'
 
 @Component({ name: 'RegionPrimersTool' })
+
 export default class RegionPrimersTool extends Vue {
-  @Prop({ required: true }) modalData!: DialogBase;
-  @Prop({ required: true }) isLoading!: boolean;
+  @Prop({ required: true }) modalData!: DialogBase
+  @Prop({ required: true }) isLoading!: boolean
 
-  studyList: string[] = [];
-  projectList: string[] = [];
-  regionList: string[] = [];
+  studyList: string[] = []
+  projectList: string[] = []
+  regionList: string[] = []
 
-  RegionPrimersToolForm: RegionPrimersTools = {
+  regionPrimersToolForm: RegionPrimersTools = {
     user: this.$store.state.user.id,
     requestType: 'AdaptoPrimerRequest',
     studyName: '',
@@ -204,77 +148,59 @@ export default class RegionPrimersTool extends Vue {
     tailMinDistance: '100',
     type: 'Adapto',
     well: 'A1'
-  };
+  }
 
   rules: object = {
     studyName: { required: true, message: 'Study is required' },
     projectName: { required: true, message: 'Project name is required' },
     dnaDesignName: { required: true, message: 'Region is required' }
-  };
+  }
 
   $refs!: {
-    RegionPrimersToolForm: HTMLFormElement;
-  };
+    regionPrimersToolForm: HTMLFormElement
+  }
 
-  getRegions () {
+  getStudyList () {
     this.$emit('loadOn')
-    return httpService
-      .post('query/locusRegionRetriever', {
-        study: this.RegionPrimersToolForm.studyName,
-        project: this.RegionPrimersToolForm.projectName
-      })
-      .then((res: any) => {
-        this.regionList = res.data.regions
-      })
-      .catch((err: any) => {
-        throw new Error(err)
-      })
+    return httpService.get('query/studyNameList')
+      .then((res: any) => { this.studyList = res.data.rows })
+      .catch((err: any) => { throw new Error(err) })
       .finally(() => this.$emit('loadOff'))
   }
 
   getProjects () {
     this.$emit('loadOn')
-    return httpService
-      .post('query/projectNameList', {
-        study: this.RegionPrimersToolForm.studyName
-      })
+    return httpService.post('query/projectNameList', { study: this.regionPrimersToolForm.studyName })
       .then((res: any) => {
+        this.regionPrimersToolForm.projectName = ''
+        this.regionPrimersToolForm.dnaDesignName = ''
         this.projectList = res.data.rows
       })
-      .catch((err: any) => {
-        throw new Error(err)
-      })
+      .catch((err: any) => { throw new Error(err) })
       .finally(() => this.$emit('loadOff'))
   }
 
-  getStudyList () {
+  getRegions () {
     this.$emit('loadOn')
-    return httpService
-      .get('query/studyNameList')
-      .then((res: any) => {
-        this.studyList = res.data.rows
-      })
-      .catch((err: any) => {
-        throw new Error(err)
-      })
+    return httpService.post('query/locusRegionRetriever', {
+      study: this.regionPrimersToolForm.studyName,
+      project: this.regionPrimersToolForm.projectName
+    }).then((res: any) => {
+      this.regionPrimersToolForm.dnaDesignName = ''
+      this.regionList = res.data.regions
+    }).catch((err: any) => { throw new Error(err) })
       .finally(() => this.$emit('loadOff'))
   }
 
   get sendData () {
-    return this.RegionPrimersToolForm
+    return this.regionPrimersToolForm
   }
 
   /* submit Modal data */
-
-  save (next?: string) {
-    this.$refs['RegionPrimersToolForm'].validate((valid: boolean) => {
-      if (valid) {
-        this.$emit(
-          'save',
-          { data: this.sendData },
-          next === 'next' ? this.modalData.saveAndNext : null
-        )
-      } else return false
+  save () {
+    this.$refs['regionPrimersToolForm'].validate((valid: boolean) => {
+      if (valid) this.$emit('save', { data: this.sendData })
+      else return false
     })
   }
 
