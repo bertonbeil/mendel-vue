c79c590f2cf8f164eff8b975990f53cca4a72373
Andrew Dmytriv <a.dmytriv@softonix.org>
2019-10-22 13:58:00 +0300

    updates for work

--- src/playground/Playground.vue
+++ src/playground/Playground.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="w-full">
+
+    <div class="w-1/2">
+      <el-select v-model="selected" placeholder="">
+        <el-option v-for="item in promoters"
+          :key="item"
+          :label="item"
+          :value="item">
+        </el-option>
+      </el-select>
+    </div>
+
     <div class="w-1/2">
       Virtual select
 
       <div class="p-30 bg-grey h-200">
         <virtual-list :size="20" :remain="8" :start="0">
-          <ItemFoo class="h-20" style="box-sizing: border-box" v-for="item of promoters" :key="item.name" :name="item.name"> </ItemFoo>
+          <ItemFoo @click.native="testIvent(item)" class="h-20" style="box-sizing: border-box" v-for="item of promoters" :key="item" :name="item"> </ItemFoo>
         </virtual-list>
       </div>
+    </div>
+
+    <div class="w-1/2">
+      <!-- vistual-select @change(return selected item) v-model/sync prop-->
     </div>
   </div>
 </template>
@@ -25,6 +40,11 @@ import { httpService } from '@/services/http.service'
 
 export default class Playground extends Vue {
   promoters = []
+  selected = ''
+
+  testIvent (item) {
+    console.log('selected item: ', item)
+  }
 
   getPromoters () {
     return httpService.get('query/promoterNameList')
@@ -32,8 +52,25 @@ export default class Playground extends Vue {
       .catch((err: any) => { throw new Error(err) })
   }
 
+  filterUniqItems () {
+    let uniqItems = new Map()
+
+    this.promoters.forEach((p: any) => {
+      uniqItems.set(p.name, p.name)
+    })
+
+    for (var value of uniqItems.values()) {
+      this.promoters.push(value)
+    }
+
+    console.log('this.promoters: ', this.promoters)
+  }
+
   created () {
     this.getPromoters()
+      .then(() => {
+        this.filterUniqItems()
+      })
   }
 
 }
